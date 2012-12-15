a24275d9fc6d9c5f44a63da163508031e4d2f634
akwizgran <michael@briarproject.org>
2012-12-15 02:55:57 +0000

    Javadocs for reliability package.

--- briar-api/src/net/sf/briar/api/reliability/ReliabilityLayerFactory.java
+++ briar-api/src/net/sf/briar/api/reliability/ReliabilityLayerFactory.java
@@ -2,5 +2,6 @@ package net.sf.briar.api.reliability;
 
 public interface ReliabilityLayerFactory {
 
+	/** Returns a reliability layer that writes to the given lower layer. */
 	ReliabilityLayer createReliabilityLayer(WriteHandler writeHandler);
 }
