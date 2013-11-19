e73dc148c3d69e3e897bcadd66b30c8ea00d569a
akwizgran <akwizgran@users.sourceforge.net>
2013-11-19 23:13:35 +0000

    Avoid using a deprecated API if possible.

--- briar-android/src/net/sf/briar/android/AndroidFileUtils.java
+++ briar-android/src/net/sf/briar/android/AndroidFileUtils.java
@@ -4,12 +4,15 @@ import java.io.File;
 import java.io.IOException;
 
 import net.sf.briar.api.os.FileUtils;
+import android.os.Build;
 import android.os.StatFs;
 
 class AndroidFileUtils implements FileUtils {
 
 	public long getFreeSpace(File f) throws IOException {
 		StatFs s = new StatFs(f.getAbsolutePath());
+		if(Build.VERSION.SDK_INT >= 18)
+			return s.getAvailableBlocksLong() * s.getBlockSizeLong();
 		return (long) s.getAvailableBlocks() * s.getBlockSize();
 	}
 }
