46863b8c1bca9865ecb7ca6f2cb259c71f8ab627
akwizgran <akwizgran@users.sourceforge.net>
2014-01-14 18:59:40 +0000

    Log how long it takes Guice to construct its object graph.

--- briar-android/src/org/briarproject/android/SplashScreenActivity.java
+++ briar-android/src/org/briarproject/android/SplashScreenActivity.java
@@ -2,8 +2,13 @@ package org.briarproject.android;
 
 import static android.content.Intent.FLAG_ACTIVITY_NEW_TASK;
 import static android.view.Gravity.CENTER;
+import static java.util.logging.Level.INFO;
 import static org.briarproject.android.util.CommonLayoutParams.MATCH_MATCH;
+
+import java.util.logging.Logger;
+
 import org.briarproject.api.db.DatabaseConfig;
+
 import roboguice.RoboGuice;
 import roboguice.activity.RoboSplashActivity;
 import android.content.Intent;
@@ -14,6 +19,11 @@ import android.widget.ProgressBar;
 import com.google.inject.Injector;
 
 public class SplashScreenActivity extends RoboSplashActivity {
+
+	private static final Logger LOG =
+			Logger.getLogger(SplashScreenActivity.class.getName());
+
+	private long start = System.currentTimeMillis();
 
 	public SplashScreenActivity() {
 		minDisplayMs = 0;
@@ -32,6 +42,9 @@ public class SplashScreenActivity extends RoboSplashActivity {
 	}
 
 	protected void startNextActivity() {
+		long duration = System.currentTimeMillis() - start;
+		if(LOG.isLoggable(INFO))
+			LOG.info("Guice startup took " + duration + " ms");
 		Injector guice = RoboGuice.getBaseApplicationInjector(getApplication());
 		if(guice.getInstance(DatabaseConfig.class).databaseExists()) {
 			Intent i = new Intent(this, HomeScreenActivity.class);
