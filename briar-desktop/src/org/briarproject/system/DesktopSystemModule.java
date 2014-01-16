66619ec1e79e687606bf01309ecd9b0a0840c21f
akwizgran <akwizgran@users.sourceforge.net>
2014-01-16 19:12:27 +0000

    Use a single Timer instance.

--- briar-desktop/src/org/briarproject/system/DesktopSystemModule.java
+++ briar-desktop/src/org/briarproject/system/DesktopSystemModule.java
@@ -15,7 +15,7 @@ public class DesktopSystemModule extends AbstractModule {
 
 	protected void configure() {
 		bind(Clock.class).to(SystemClock.class);
-		bind(Timer.class).to(SystemTimer.class);
+		bind(Timer.class).toInstance(new SystemTimer());
 		if(OsUtils.isLinux())
 			bind(SeedProvider.class).to(LinuxSeedProvider.class);
 		bind(FileUtils.class).toInstance(new FileUtils() {
