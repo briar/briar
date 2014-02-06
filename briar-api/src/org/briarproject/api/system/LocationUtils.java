48e5d5123e93966c52b82330e954a23d3f8f5dad
Ximin Luo <infinity0@pwned.gg>
2014-02-06 19:18:19 +0000

    add documentation and pointer to hidden Google API - fallback to SIM card before phone locale - add disabled code to lookup the country from GPS, disabled because it requires a network call

--- briar-api/src/org/briarproject/api/system/LocationUtils.java
+++ briar-api/src/org/briarproject/api/system/LocationUtils.java
@@ -2,7 +2,12 @@ package org.briarproject.api.system;
 
 public interface LocationUtils {
 	
-	/** Get the country the device is currently-located in. */
+	/** Get the country the device is currently-located in, or "" if it cannot
+	 * be determined. Should never return {@code null}.
+	 *
+	 * <p>The country codes are formatted upper-case and as per <a href="
+	 * https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2">ISO 3166-1 alpha 2</a>.
+	 */
 	String getCurrentCountry();
 
 }
