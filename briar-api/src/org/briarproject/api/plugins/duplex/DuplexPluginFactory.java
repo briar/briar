250afa7642a530e7c19ca491026b741815be764a
akwizgran <akwizgran@users.sourceforge.net>
2014-03-06 13:20:28 +0000

    Added javadoc comments to plugin factory interfaces.

--- briar-api/src/org/briarproject/api/plugins/duplex/DuplexPluginFactory.java
+++ briar-api/src/org/briarproject/api/plugins/duplex/DuplexPluginFactory.java
@@ -2,9 +2,12 @@ package org.briarproject.api.plugins.duplex;
 
 import org.briarproject.api.TransportId;
 
+/** Factory for creating a plugin for a duplex transport. */
 public interface DuplexPluginFactory {
 
+	/** Returns the plugin's transport identifier. */
 	TransportId getId();
 
+	/** Creates and returns a plugin, or null if no plugin can be created. */
 	DuplexPlugin createPlugin(DuplexPluginCallback callback);
 }
--- briar-api/src/org/briarproject/api/plugins/simplex/SimplexPluginFactory.java
+++ briar-api/src/org/briarproject/api/plugins/simplex/SimplexPluginFactory.java
@@ -2,9 +2,12 @@ package org.briarproject.api.plugins.simplex;
 
 import org.briarproject.api.TransportId;
 
+/** Factory for creating a plugin for a simplex transport. */
 public interface SimplexPluginFactory {
 
+	/** Returns the plugin's transport identifier. */
 	TransportId getId();
 
+	/** Creates and returns a plugin, or null if no plugin can be created. */
 	SimplexPlugin createPlugin(SimplexPluginCallback callback);
 }
