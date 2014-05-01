d5bf5871a909746318d59a6c52951d3a7b7056c2
akwizgran <akwizgran@users.sourceforge.net>
2014-05-01 14:18:21 +0100

    Suppress lint warning: NewApi.

--- briar-android/src/org/briarproject/system/AndroidFileUtils.java
+++ briar-android/src/org/briarproject/system/AndroidFileUtils.java
@@ -2,14 +2,14 @@ package org.briarproject.system;
 
 import java.io.File;
 import java.io.IOException;
-
 import org.briarproject.api.system.FileUtils;
-
+import android.annotation.SuppressLint;
 import android.os.Build;
 import android.os.StatFs;
 
 class AndroidFileUtils implements FileUtils {
 
+	@SuppressLint("NewApi")
 	@SuppressWarnings("deprecation")
 	public long getTotalSpace(File f) throws IOException {
 		if(Build.VERSION.SDK_INT >= 9) return f.getTotalSpace();
@@ -18,6 +18,7 @@ class AndroidFileUtils implements FileUtils {
 		return (long) s.getBlockCount() * s.getBlockSize();
 	}
 
+	@SuppressLint("NewApi")
 	@SuppressWarnings("deprecation")
 	public long getFreeSpace(File f) throws IOException {
 		if(Build.VERSION.SDK_INT >= 9) return f.getUsableSpace();
