cdf8392725c87ac418bc1f32379d9820fbcea0c2
Ernir Erlingsson <ernir@ymirmobile.com>
2015-12-14 22:22:49 +0100

    code nitpicks and color fix

--- briar-android/src/org/briarproject/android/BaseActivity.java
+++ briar-android/src/org/briarproject/android/BaseActivity.java
@@ -47,42 +47,42 @@ public abstract class BaseActivity extends AppCompatActivity implements RoboCont
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		RoboInjector injector = RoboGuice.getInjector(this);
-		this.eventManager = (EventManager) injector.getInstance(EventManager.class);
+		eventManager = (EventManager) injector.getInstance(EventManager.class);
 		injector.injectMembersWithoutViews(this);
 		super.onCreate(savedInstanceState);
-		this.eventManager.fire(new OnCreateEvent(savedInstanceState));
+		eventManager.fire(new OnCreateEvent(savedInstanceState));
 
 		if (PREVENT_SCREENSHOTS) getWindow().addFlags(FLAG_SECURE);
 	}
 
 	protected void onRestart() {
 		super.onRestart();
-		this.eventManager.fire(new OnRestartEvent());
+		eventManager.fire(new OnRestartEvent());
 	}
 
 	protected void onStart() {
 		super.onStart();
-		this.eventManager.fire(new OnStartEvent());
+		eventManager.fire(new OnStartEvent());
 	}
 
 	protected void onResume() {
 		super.onResume();
-		this.eventManager.fire(new OnResumeEvent());
+		eventManager.fire(new OnResumeEvent());
 	}
 
 	protected void onPause() {
 		super.onPause();
-		this.eventManager.fire(new OnPauseEvent());
+		eventManager.fire(new OnPauseEvent());
 	}
 
 	protected void onNewIntent(Intent intent) {
 		super.onNewIntent(intent);
-		this.eventManager.fire(new OnNewIntentEvent());
+		eventManager.fire(new OnNewIntentEvent());
 	}
 
 	protected void onStop() {
 		try {
-			this.eventManager.fire(new OnStopEvent());
+			eventManager.fire(new OnStopEvent());
 		} finally {
 			super.onStop();
 		}
@@ -91,7 +91,7 @@ public abstract class BaseActivity extends AppCompatActivity implements RoboCont
 
 	protected void onDestroy() {
 		try {
-			this.eventManager.fire(new OnDestroyEvent());
+			eventManager.fire(new OnDestroyEvent());
 		} finally {
 			try {
 				RoboGuice.destroyInjector(this);
@@ -103,25 +103,25 @@ public abstract class BaseActivity extends AppCompatActivity implements RoboCont
 	}
 
 	public void onConfigurationChanged(Configuration newConfig) {
-		Configuration currentConfig = this.getResources().getConfiguration();
+		Configuration currentConfig = getResources().getConfiguration();
 		super.onConfigurationChanged(newConfig);
-		this.eventManager.fire(new OnConfigurationChangedEvent(currentConfig, newConfig));
+		eventManager.fire(new OnConfigurationChangedEvent(currentConfig, newConfig));
 	}
 
 	public void onContentChanged() {
 		super.onContentChanged();
 		RoboGuice.getInjector(this).injectViewMembers(this);
-		this.eventManager.fire(new OnContentChangedEvent());
+		eventManager.fire(new OnContentChangedEvent());
 	}
 
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		super.onActivityResult(requestCode, resultCode, data);
-		this.eventManager.fire(new OnActivityResultEvent(requestCode, resultCode, data));
+		eventManager.fire(new OnActivityResultEvent(requestCode, resultCode, data));
 	}
 
 	@Override
 	public Map<Key<?>, Object> getScopedObjectMap() {
-		return this.scopedObjects;
+		return scopedObjects;
 	}
 
 	private SharedPreferences getBriarPrefs(String prefsName) {
