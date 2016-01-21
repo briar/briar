dd8e111b0b85de24af53f50d243344d8484ce739
akwizgran <akwizgran@users.sourceforge.net>
2016-01-21 14:41:17 +0000

    Add Settings.Secure.ANDROID_ID to entropy pool.

--- briar-android/src/org/briarproject/system/AndroidSeedProvider.java
+++ briar-android/src/org/briarproject/system/AndroidSeedProvider.java
@@ -1,11 +1,26 @@
 package org.briarproject.system;
 
+import android.app.Application;
+import android.content.ContentResolver;
+import android.content.Context;
 import android.os.Build;
+import android.provider.Settings;
 
 import java.io.DataOutputStream;
 import java.io.IOException;
 
+import javax.inject.Inject;
+
+import static android.provider.Settings.Secure.ANDROID_ID;
+
 class AndroidSeedProvider extends LinuxSeedProvider {
+
+	private final Context appContext;
+
+	@Inject
+	AndroidSeedProvider(Application app) {
+		appContext = app.getApplicationContext();
+	}
 
 	@Override
 	void writeToEntropyPool(DataOutputStream out) throws IOException {
@@ -14,6 +29,8 @@ class AndroidSeedProvider extends LinuxSeedProvider {
 		out.writeInt(android.os.Process.myUid());
 		if (Build.FINGERPRINT != null) out.writeUTF(Build.FINGERPRINT);
 		if (Build.SERIAL != null) out.writeUTF(Build.SERIAL);
+		ContentResolver contentResolver = appContext.getContentResolver();
+		out.writeUTF(Settings.Secure.getString(contentResolver, ANDROID_ID));
 		super.writeToEntropyPool(out);
 	}
 }
