f2e1723b24ea1a7c970112fdc71f90c5129625d0
akwizgran <akwizgran@users.sourceforge.net>
2016-01-26 10:31:32 +0000

    Added thread safety documentation to interfaces.

--- briar-api/src/org/briarproject/api/event/EventListener.java
+++ briar-api/src/org/briarproject/api/event/EventListener.java
@@ -3,5 +3,9 @@ package org.briarproject.api.event;
 /** An interface for receiving notifications when events occur. */
 public interface EventListener {
 
+	/**
+	 * Called when an event is broadcast. Implementations of this method must
+	 * not block.
+	 */
 	void eventOccurred(Event e);
 }
