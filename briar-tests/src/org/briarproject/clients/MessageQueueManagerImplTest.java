9e40d0be16bab5fa25bfd6f0348302797a9d2ffc
akwizgran <akwizgran@users.sourceforge.net>
2016-03-08 16:28:31 +0000

    Added placeholder for MessageQueueManagerImpl tests.

---
+++ briar-tests/src/org/briarproject/clients/MessageQueueManagerImplTest.java
@@ -0,0 +1,14 @@
+package org.briarproject.clients;
+
+import org.briarproject.BriarTestCase;
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+public class MessageQueueManagerImplTest extends BriarTestCase {
+
+	@Test
+	public void testUnitTestsExist() {
+		fail(); // FIXME: Write tests
+	}
+}
