30092550c751c724e29dbd0969358f40d85be8d9
Torsten Grote <t@grobox.de>
2016-08-08 13:00:11 -0300

    Don't crash when pressing Up button in WriteBlogPostActivity
    Closes #587

--- briar-android/src/org/briarproject/android/blogs/WriteBlogPostActivity.java
+++ briar-android/src/org/briarproject/android/blogs/WriteBlogPostActivity.java
@@ -7,6 +7,7 @@ import android.support.design.widget.TextInputLayout;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.KeyEvent;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -113,6 +114,17 @@ public class WriteBlogPostActivity extends BriarActivity
 		});
 
 		progressBar = (ProgressBar) findViewById(R.id.progressBar);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+			case android.R.id.home:
+				onBackPressed();
+				return true;
+			default:
+				return super.onOptionsItemSelected(item);
+		}
 	}
 
 	@Override
