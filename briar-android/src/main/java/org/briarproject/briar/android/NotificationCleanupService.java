53f05a72ba068be34e57cccf7abbc4d0a6ea261e
akwizgran <akwizgran@users.sourceforge.net>
2017-09-29 15:31:25 +0100

    Removed logging.

--- briar-android/src/main/java/org/briarproject/briar/android/NotificationCleanupService.java
+++ briar-android/src/main/java/org/briarproject/briar/android/NotificationCleanupService.java
@@ -3,7 +3,6 @@ package org.briarproject.briar.android;
 import android.app.IntentService;
 import android.content.Intent;
 import android.support.annotation.Nullable;
-import android.util.Log;
 
 import org.briarproject.briar.api.android.AndroidNotificationManager;
 
@@ -37,28 +36,18 @@ public class NotificationCleanupService extends IntentService {
 
 	@Override
 	protected void onHandleIntent(@Nullable Intent i) {
-		if (i == null || i.getData() == null) {
-			Log.i(TAG, "No intent or no data");
-			return;
-		}
+		if (i == null || i.getData() == null) return;
 		String uri = i.getData().toString();
 		if (uri.equals(CONTACT_URI)) {
-			Log.i(TAG, "Clearing contact notifications");
 			notificationManager.clearAllContactNotifications();
 		} else if (uri.equals(GROUP_URI)) {
-			Log.i(TAG, "Clearing group notifications");
 			notificationManager.clearAllGroupMessageNotifications();
 		} else if (uri.equals(FORUM_URI)) {
-			Log.i(TAG, "Clearing forum notifications");
 			notificationManager.clearAllForumPostNotifications();
 		} else if (uri.equals(BLOG_URI)) {
-			Log.i(TAG, "Clearing blog notifications");
 			notificationManager.clearAllBlogPostNotifications();
 		} else if (uri.equals(INTRODUCTION_URI)) {
-			Log.i(TAG, "Clearing introduction notifications");
 			notificationManager.clearAllIntroductionNotifications();
-		} else {
-			Log.w(TAG, "Unknown intent URI: " + uri);
 		}
 	}
 }
