5883eee42fc96192fddee813e9e43cf818cf834b
akwizgran <michael@briarproject.org>
2018-04-19 17:39:07 +0100

    Add download button to ExpiredActivity.

--- briar-android/src/main/java/org/briarproject/briar/android/splash/ExpiredActivity.java
+++ briar-android/src/main/java/org/briarproject/briar/android/splash/ExpiredActivity.java
@@ -1,14 +1,20 @@
 package org.briarproject.briar.android.splash;
 
-import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.View.OnClickListener;
 
 import org.briarproject.briar.R;
 
+import static android.content.Intent.ACTION_VIEW;
 import static android.view.WindowManager.LayoutParams.FLAG_SECURE;
 import static org.briarproject.briar.android.TestingConstants.PREVENT_SCREENSHOTS;
 
-public class ExpiredActivity extends Activity {
+public class ExpiredActivity extends AppCompatActivity
+		implements OnClickListener {
 
 	@Override
 	public void onCreate(Bundle state) {
@@ -17,5 +23,13 @@ public class ExpiredActivity extends Activity {
 		if (PREVENT_SCREENSHOTS) getWindow().addFlags(FLAG_SECURE);
 
 		setContentView(R.layout.activity_expired);
+		findViewById(R.id.download_briar_button).setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View v) {
+		Uri uri = Uri.parse("https://briarproject.org/download.html");
+		startActivity(new Intent(ACTION_VIEW, uri));
+		finish();
 	}
 }
