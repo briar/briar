a1599df712f79108be180c029aa5ba4a73892850
akwizgran <michael@briarproject.org>
2018-08-10 15:04:08 +0100

    Remove unused constants.

--- bramble-api/src/main/java/org/briarproject/bramble/api/keyagreement/KeyAgreementConstants.java
+++ bramble-api/src/main/java/org/briarproject/bramble/api/keyagreement/KeyAgreementConstants.java
@@ -8,16 +8,6 @@ public interface KeyAgreementConstants {
 	byte PROTOCOL_VERSION = 4;
 
 	/**
-	 * The length of the record header in bytes.
-	 */
-	int RECORD_HEADER_LENGTH = 4;
-
-	/**
-	 * The offset of the payload length in the record header, in bytes.
-	 */
-	int RECORD_HEADER_PAYLOAD_LENGTH_OFFSET = 2;
-
-	/**
 	 * The length of the BQP key commitment in bytes.
 	 */
 	int COMMIT_LENGTH = 16;
