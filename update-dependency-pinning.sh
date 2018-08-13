30e036c6728f182fe1fa00cdfb04c6034cbd47b7
Torsten Grote <t@grobox.de>
2018-08-13 14:58:06 -0300

    Move gradle witness dependency verification into dedicated files
    This also adds a script for makes upgrading dependency checksums so much easier

---
+++ update-dependency-pinning.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e
+
+PROJECTS=(
+    'bramble-api'
+    'bramble-core'
+    'bramble-android'
+    'bramble-j2se'
+    'briar-api'
+    'briar-core'
+    'briar-android'
+)
+
+# clear witness files to prevent errors when upgrading dependencies
+for project in ${PROJECTS[@]}
+do
+    echo "" > ${project}/witness.gradle
+done
+
+# calculating new checksums
+for project in ${PROJECTS[@]}
+do
+    echo "Calculating new checksums for ${project} ..."
+    gradle -q ${project}:calculateChecksums | grep -v '^Verifying' > ${project}/witness.gradle
+done
