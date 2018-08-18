d11f1d28053be5195382f5938bac8cb8b52fc4c0
Torsten Grote <t@grobox.de>
2018-08-18 17:30:45 -0300

    Add a poor man's argument to explicitly turn on verbose logging

--- briar-headless/src/main/java/org/briarproject/briar/headless/Main.java
+++ briar-headless/src/main/java/org/briarproject/briar/headless/Main.java
@@ -1,8 +1,21 @@
 package org.briarproject.briar.headless;
 
+import java.util.logging.Level;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+
 public class Main {
 
 	public static void main(String[] args) {
+		Logger rootLogger = LogManager.getLogManager().getLogger("");
+		rootLogger.setLevel(Level.WARNING);
+
+		for (String arg : args) {
+			if (arg.equals("-v")) {
+				rootLogger.setLevel(Level.INFO);
+			}
+		}
+
 		DaggerBriarHeadlessApp
 				.create()
 				.router()
