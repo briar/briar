ad4e8d51e90bfc17bf53d8cdd14d7293d40c8c04
akwizgran <michael@briarproject.org>
2018-08-21 10:01:07 +0100

    Hide soft keyboard when sending blog post.

--- briar-android/src/main/java/org/briarproject/briar/android/blog/WriteBlogPostActivity.java
+++ briar-android/src/main/java/org/briarproject/briar/android/blog/WriteBlogPostActivity.java
@@ -134,6 +134,7 @@ public class WriteBlogPostActivity extends BriarActivity
 	@Override
 	public void onSendClick(String body) {
 		// hide publish button, show progress bar
+		hideSoftKeyboard(input);
 		input.setVisibility(GONE);
 		progressBar.setVisibility(VISIBLE);
 
