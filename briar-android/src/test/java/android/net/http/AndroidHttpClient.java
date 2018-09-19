b101c4b636097aca743d899905245d5a3849fb19
Torsten Grote <t@grobox.de>
2018-09-19 15:15:38 -0300

    Remove AndroidHttpClient class after Roboelectric update

--- briar-android/src/test/java/android/net/http/AndroidHttpClient.java
+++
@@ -1,7 +0,0 @@
-package android.net.http;
-
-// This class is here to fix an issue with Robolectric.
-// https://github.com/robolectric/robolectric/issues/1862
-// TODO: Check if this class can be removed on next Robolectric update
-public class AndroidHttpClient {
-}
