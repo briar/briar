d07c1443161f2da3b06a295d049483e8329e2c47
akwizgran <michael@briarproject.org>
2018-10-16 14:50:59 +0100

    Remove unnecessary null check.

--- bramble-core/src/main/java/org/briarproject/bramble/db/JdbcDatabase.java
+++ bramble-core/src/main/java/org/briarproject/bramble/db/JdbcDatabase.java
@@ -766,7 +766,7 @@ abstract class JdbcDatabase implements Database<Connection> {
 			for (Entry<ContactId, Boolean> e : visibility.entrySet()) {
 				ContactId c = e.getKey();
 				boolean offered = removeOfferedMessage(txn, c, m.getId());
-				boolean seen = offered || (sender != null && c.equals(sender));
+				boolean seen = offered || c.equals(sender);
 				addStatus(txn, m.getId(), c, m.getGroupId(), m.getTimestamp(),
 						raw.length, state, e.getValue(), messageShared,
 						false, seen);
