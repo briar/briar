fb1d8e860f1345970e532ca18d74403195291ed8
Torsten Grote <t@grobox.de>
2018-11-30 12:25:16 -0200

    [api] Add interface for adding contacts remotely

---
+++ bramble-api/src/main/java/org/briarproject/bramble/api/contact/event/PendingContactStateChangedEvent.java
@@ -0,0 +1,34 @@
+package org.briarproject.bramble.api.contact.event;
+
+import org.briarproject.bramble.api.contact.PendingContact.PendingContactState;
+import org.briarproject.bramble.api.contact.PendingContactId;
+import org.briarproject.bramble.api.event.Event;
+import org.briarproject.bramble.api.nullsafety.NotNullByDefault;
+
+import javax.annotation.concurrent.Immutable;
+
+/**
+ * An event that is broadcast when a pending contact's state is changed.
+ */
+@Immutable
+@NotNullByDefault
+public class PendingContactStateChangedEvent extends Event {
+
+	private final PendingContactId id;
+	private final PendingContactState state;
+
+	public PendingContactStateChangedEvent(PendingContactId id,
+			PendingContactState state) {
+		this.id = id;
+		this.state = state;
+	}
+
+	public PendingContactId getId() {
+		return id;
+	}
+
+	public PendingContactState getPendingContactState() {
+		return state;
+	}
+
+}
