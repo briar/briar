91777fd94249531fdb599e652c2efc33d8626079
akwizgran <michael@briarproject.org>
2018-11-30 11:33:27 +0000

    Hide UI without flashing.

--- briar-android/src/main/java/org/briarproject/briar/android/logout/HideUiActivity.java
+++ briar-android/src/main/java/org/briarproject/briar/android/logout/HideUiActivity.java
@@ -1,20 +1,13 @@
 package org.briarproject.briar.android.logout;
 
+import android.app.Activity;
 import android.os.Bundle;
 
-import org.briarproject.briar.android.activity.ActivityComponent;
-import org.briarproject.briar.android.activity.BaseActivity;
-
-public class HideUiActivity extends BaseActivity {
+public class HideUiActivity extends Activity {
 
 	@Override
 	public void onCreate(Bundle state) {
 		super.onCreate(state);
 		finish();
-	}
-
-	@Override
-	public void injectActivity(ActivityComponent component) {
-
 	}
 }
