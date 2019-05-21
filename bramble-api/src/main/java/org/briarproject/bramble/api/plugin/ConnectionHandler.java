ead7570ec59b624df8b0d7a6c927ba7e6f506e72
akwizgran <michael@briarproject.org>
2019-05-21 14:27:27 +0100

    Add javadoc.

--- bramble-api/src/main/java/org/briarproject/bramble/api/plugin/ConnectionHandler.java
+++ bramble-api/src/main/java/org/briarproject/bramble/api/plugin/ConnectionHandler.java
@@ -1,7 +1,9 @@
 package org.briarproject.bramble.api.plugin;
 
 import org.briarproject.bramble.api.nullsafety.NotNullByDefault;
+import org.briarproject.bramble.api.plugin.duplex.DuplexPlugin;
 import org.briarproject.bramble.api.plugin.duplex.DuplexTransportConnection;
+import org.briarproject.bramble.api.plugin.simplex.SimplexPlugin;
 
 /**
  * An interface for handling connections created by transport plugins.
@@ -9,9 +11,18 @@ import org.briarproject.bramble.api.plugin.duplex.DuplexTransportConnection;
 @NotNullByDefault
 public interface ConnectionHandler {
 
+	/**
+	 * Handles a connection created by a {@link DuplexPlugin}.
+	 */
 	void handleConnection(DuplexTransportConnection c);
 
+	/**
+	 * Handles a reader created by a {@link SimplexPlugin}.
+	 */
 	void handleReader(TransportConnectionReader r);
 
+	/**
+	 * Handles a writer created by a {@link SimplexPlugin}.
+	 */
 	void handleWriter(TransportConnectionWriter w);
 }
