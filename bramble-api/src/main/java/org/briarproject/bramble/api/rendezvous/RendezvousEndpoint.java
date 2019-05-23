7fd8ad65be2832c280302e313ad7c220131bfe0a
akwizgran <michael@briarproject.org>
2019-05-23 12:41:44 +0100

    Make RendezvousEndpoint closeable.

--- bramble-api/src/main/java/org/briarproject/bramble/api/rendezvous/RendezvousEndpoint.java
+++ bramble-api/src/main/java/org/briarproject/bramble/api/rendezvous/RendezvousEndpoint.java
@@ -2,13 +2,14 @@ package org.briarproject.bramble.api.rendezvous;
 
 import org.briarproject.bramble.api.properties.TransportProperties;
 
+import java.io.Closeable;
 import java.io.IOException;
 
 /**
  * An interface for making and accepting rendezvous connections with a pending
  * contact over a given transport.
  */
-public interface RendezvousEndpoint {
+public interface RendezvousEndpoint extends Closeable {
 
 	/**
 	 * Returns a set of transport properties for connecting to the pending
@@ -20,5 +21,6 @@ public interface RendezvousEndpoint {
 	 * Closes the handler and releases any resources held by it, such as
 	 * network sockets.
 	 */
+	@Override
 	void close() throws IOException;
 }
