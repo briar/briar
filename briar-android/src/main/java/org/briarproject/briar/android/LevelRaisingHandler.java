be76c5b7db9777a14096e98dddeae4f9a8d017ea
akwizgran <michael@briarproject.org>
2019-07-01 14:38:28 +0100

    Add safety annotations.

--- briar-android/src/main/java/org/briarproject/briar/android/LevelRaisingHandler.java
+++ briar-android/src/main/java/org/briarproject/briar/android/LevelRaisingHandler.java
@@ -1,13 +1,19 @@
 package org.briarproject.briar.android;
 
+import org.briarproject.bramble.api.nullsafety.NotNullByDefault;
+
 import java.util.logging.Handler;
 import java.util.logging.Level;
 import java.util.logging.LogRecord;
+
+import javax.annotation.concurrent.Immutable;
 
 /**
  * Log handler that raises all records at or above a given source level to a
  * given destination level. This affects the level seen by subsequent handlers.
  */
+@Immutable
+@NotNullByDefault
 class LevelRaisingHandler extends Handler {
 
 	private final Level dest;
