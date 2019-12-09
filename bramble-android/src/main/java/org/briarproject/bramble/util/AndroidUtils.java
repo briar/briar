d1e21877b3d83850376df1795b94d4bda2dfe876
akwizgran <michael@briarproject.org>
2019-12-09 13:59:11 +0000

    Use reflection to get local Bluetooth address.
    This is expected to work on Android 8 but not 8.1+.

--- bramble-android/src/main/java/org/briarproject/bramble/util/AndroidUtils.java
+++ bramble-android/src/main/java/org/briarproject/bramble/util/AndroidUtils.java
@@ -6,15 +6,25 @@ import android.content.Context;
 import android.os.Build;
 import android.provider.Settings;
 
+import org.briarproject.bramble.api.Pair;
+import org.briarproject.bramble.api.nullsafety.NotNullByDefault;
+
 import java.io.File;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 
+import javax.annotation.Nullable;
+
 import static android.content.Context.MODE_PRIVATE;
 import static android.os.Build.VERSION.SDK_INT;
+import static java.util.Arrays.asList;
+import static org.briarproject.bramble.api.nullsafety.NullSafety.requireNonNull;
 
+@NotNullByDefault
 public class AndroidUtils {
 
 	// Fake Bluetooth address returned by BluetoothAdapter on API 23 and later
@@ -22,11 +32,10 @@ public class AndroidUtils {
 
 	private static final String STORED_REPORTS = "dev-reports";
 
-	@SuppressWarnings("deprecation")
 	public static Collection<String> getSupportedArchitectures() {
 		List<String> abis = new ArrayList<>();
 		if (SDK_INT >= 21) {
-			abis.addAll(Arrays.asList(Build.SUPPORTED_ABIS));
+			abis.addAll(asList(Build.SUPPORTED_ABIS));
 		} else {
 			abis.add(Build.CPU_ABI);
 			if (Build.CPU_ABI2 != null) abis.add(Build.CPU_ABI2);
@@ -36,22 +45,60 @@ public class AndroidUtils {
 
 	public static String getBluetoothAddress(Context ctx,
 			BluetoothAdapter adapter) {
+		return getBluetoothAddressAndMethod(ctx, adapter).getFirst();
+	}
+
+	public static Pair<String, String> getBluetoothAddressAndMethod(Context ctx,
+			BluetoothAdapter adapter) {
 		// Return the adapter's address if it's valid and not fake
 		@SuppressLint("HardwareIds")
 		String address = adapter.getAddress();
-		if (isValidBluetoothAddress(address)) return address;
+		if (isValidBluetoothAddress(address)) {
+			return new Pair<>(address, "adapter");
+		}
 		// Return the address from settings if it's valid and not fake
 		address = Settings.Secure.getString(ctx.getContentResolver(),
 				"bluetooth_address");
-		if (isValidBluetoothAddress(address)) return address;
+		if (isValidBluetoothAddress(address)) {
+			return new Pair<>(address, "settings");
+		}
+		// Try to get the address via reflection
+		address = getBluetoothAddressByReflection(adapter);
+		if (isValidBluetoothAddress(address)) {
+			return new Pair<>(requireNonNull(address), "reflection");
+		}
 		// Let the caller know we can't find the address
-		return "";
+		return new Pair<>("", "");
 	}
 
-	private static boolean isValidBluetoothAddress(String address) {
+	private static boolean isValidBluetoothAddress(@Nullable String address) {
 		return !StringUtils.isNullOrEmpty(address)
 				&& BluetoothAdapter.checkBluetoothAddress(address)
 				&& !address.equals(FAKE_BLUETOOTH_ADDRESS);
+	}
+
+	@Nullable
+	private static String getBluetoothAddressByReflection(
+			BluetoothAdapter adapter) {
+		try {
+			Field mServiceField =
+					adapter.getClass().getDeclaredField("mService");
+			mServiceField.setAccessible(true);
+			Object mService = mServiceField.get(adapter);
+			// mService may be null when Bluetooth is disabled
+			if (mService == null) throw new NoSuchFieldException();
+			Method getAddressMethod =
+					mService.getClass().getMethod("getAddress");
+			return (String) getAddressMethod.invoke(mService);
+		} catch (NoSuchFieldException e) {
+			return null;
+		} catch (IllegalAccessException e) {
+			return null;
+		} catch (NoSuchMethodException e) {
+			return null;
+		} catch (InvocationTargetException e) {
+			return null;
+		}
 	}
 
 	public static File getReportDir(Context ctx) {
