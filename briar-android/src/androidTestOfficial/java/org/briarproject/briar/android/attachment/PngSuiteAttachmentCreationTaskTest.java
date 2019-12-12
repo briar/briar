ace1d38966cc5a8b60052c934fdb5eddfbe9a3bf
akwizgran <michael@briarproject.org>
2019-12-12 13:24:01 +0000

    Disable PngSuite test by default.
    Results are device-dependent and outside our
    control.

--- briar-android/src/androidTestOfficial/java/org/briarproject/briar/android/attachment/PngSuiteAttachmentCreationTaskTest.java
+++ briar-android/src/androidTestOfficial/java/org/briarproject/briar/android/attachment/PngSuiteAttachmentCreationTaskTest.java
@@ -6,18 +6,30 @@ import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
 
-import static java.util.Arrays.asList;
+import static java.util.logging.Logger.getLogger;
 import static org.briarproject.bramble.api.nullsafety.NullSafety.requireNonNull;
+import static org.briarproject.bramble.test.TestUtils.isOptionalTestEnabled;
 import static org.junit.Assert.fail;
+import static org.junit.Assume.assumeTrue;
 
 @RunWith(Parameterized.class)
 public class PngSuiteAttachmentCreationTaskTest
 		extends AbstractAttachmentCreationTaskTest {
 
+	private static final Logger LOG =
+			getLogger(PngSuiteAttachmentCreationTaskTest.class.getName());
+
 	@Parameters
 	public static Iterable<String> data() throws IOException {
-		return asList(requireNonNull(getAssetManager().list("PngSuite")));
+		List<String> data = new ArrayList<>();
+		String[] files = requireNonNull(getAssetManager().list("PngSuite"));
+		for (String filename : files)
+			if (filename.endsWith(".png")) data.add(filename);
+		return data;
 	}
 
 	private final String filename;
@@ -28,6 +40,9 @@ public class PngSuiteAttachmentCreationTaskTest
 
 	@Test
 	public void testPngSuiteCompress() throws Exception {
+		assumeTrue(isOptionalTestEnabled(
+				PngSuiteAttachmentCreationTaskTest.class));
+		LOG.info("Testing " + filename);
 		if (filename.startsWith("x")) {
 			try {
 				testCompress("PngSuite/" + filename, "image/png");
