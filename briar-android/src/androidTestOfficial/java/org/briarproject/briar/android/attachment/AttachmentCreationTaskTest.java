24a63b08c0fc3232381c800227961a59d1965811
akwizgran <michael@briarproject.org>
2019-12-12 13:23:14 +0000

    Add transparent PNG.

--- briar-android/src/androidTestOfficial/java/org/briarproject/briar/android/attachment/AttachmentCreationTaskTest.java
+++ briar-android/src/androidTestOfficial/java/org/briarproject/briar/android/attachment/AttachmentCreationTaskTest.java
@@ -3,34 +3,33 @@ package org.briarproject.briar.android.attachment;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import java.util.logging.Logger;
-
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import static android.os.Build.VERSION.SDK_INT;
-import static java.util.logging.Logger.getLogger;
 import static org.junit.Assume.assumeTrue;
 
 @RunWith(AndroidJUnit4.class)
 public class AttachmentCreationTaskTest
 		extends AbstractAttachmentCreationTaskTest {
 
-	private static Logger LOG =
-			getLogger(AttachmentCreationTaskTest.class.getName());
-
 	@Test
-	public void testCompress100x100Png() throws Exception {
+	public void testCompressSmallPng() throws Exception {
 		testCompress("red-100x100.png", "image/png");
 	}
 
 	@Test
-	public void testCompress500x500Png() throws Exception {
+	public void testCompressMediumPng() throws Exception {
 		testCompress("blue-500x500.png", "image/png");
 	}
 
 	@Test
-	public void testCompress1000x2000Png() throws Exception {
+	public void testCompressLargePng() throws Exception {
 		testCompress("green-1000x2000.png", "image/png");
+	}
+
+	@Test
+	public void testCompressTransparentPng() throws Exception {
+		testCompress("transparent-100x100.png", "image/png");
 	}
 
 	@Test
