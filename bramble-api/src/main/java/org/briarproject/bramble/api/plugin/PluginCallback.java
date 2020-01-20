e6ac6913a7dc0deb523821d66bb4df217b9e7af7
akwizgran <michael@briarproject.org>
2020-01-20 14:48:33 +0000

    Update javadocs for lock-safe methods.

--- bramble-api/src/main/java/org/briarproject/bramble/api/plugin/PluginCallback.java
+++ bramble-api/src/main/java/org/briarproject/bramble/api/plugin/PluginCallback.java
@@ -2,6 +2,9 @@ package org.briarproject.bramble.api.plugin;
 
 import org.briarproject.bramble.api.nullsafety.NotNullByDefault;
 import org.briarproject.bramble.api.plugin.Plugin.State;
+import org.briarproject.bramble.api.plugin.event.TransportActiveEvent;
+import org.briarproject.bramble.api.plugin.event.TransportInactiveEvent;
+import org.briarproject.bramble.api.plugin.event.TransportStateEvent;
 import org.briarproject.bramble.api.properties.TransportProperties;
 import org.briarproject.bramble.api.settings.Settings;
 
@@ -33,7 +36,17 @@ public interface PluginCallback extends ConnectionHandler {
 	void mergeLocalProperties(TransportProperties p);
 
 	/**
-	 * Signals that the transport's state may have changed.
+	 * Informs the callback of the plugin's current state.
+	 * <p>
+	 * If the current state is different from the previous state, the callback
+	 * will broadcast a {@link TransportStateEvent}. If the current state is
+	 * {@link State#ACTIVE} and the previous state was not
+	 * {@link State#ACTIVE}, the callback will broadcast a
+	 * {@link TransportActiveEvent}. If the current state is not
+	 * {@link State#ACTIVE} and the previous state was {@link State#ACTIVE},
+	 * the callback will broadcast a {@link TransportInactiveEvent}.
+	 * <p>
+	 * This method can safely be called while holding a lock.
 	 */
 	void pluginStateChanged(State state);
 }
