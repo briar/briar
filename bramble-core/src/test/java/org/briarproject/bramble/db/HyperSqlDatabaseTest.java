2201585a34b6f02143db09d0732a9f1e2f621313
akwizgran <michael@briarproject.org>
2020-09-10 15:07:38 +0100

    Skip HyperSQL tests if the test environment has crypto restrictions.

--- bramble-core/src/test/java/org/briarproject/bramble/db/HyperSqlDatabaseTest.java
+++ bramble-core/src/test/java/org/briarproject/bramble/db/HyperSqlDatabaseTest.java
@@ -3,8 +3,17 @@ package org.briarproject.bramble.db;
 import org.briarproject.bramble.api.db.DatabaseConfig;
 import org.briarproject.bramble.api.sync.MessageFactory;
 import org.briarproject.bramble.api.system.Clock;
+import org.junit.Before;
+
+import static org.briarproject.bramble.test.TestUtils.isCryptoStrengthUnlimited;
+import static org.junit.Assume.assumeTrue;
 
 public class HyperSqlDatabaseTest extends JdbcDatabaseTest {
+
+	@Before
+	public void setUp() {
+		assumeTrue(isCryptoStrengthUnlimited());
+	}
 
 	@Override
 	protected JdbcDatabase createDatabase(DatabaseConfig config,
