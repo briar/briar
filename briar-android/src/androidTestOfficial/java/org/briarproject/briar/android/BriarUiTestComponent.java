c032befe6f7712ae0c096c92cdb1679cbf1866d2
akwizgran <michael@briarproject.org>
2021-01-20 14:34:53 +0000

    Provide clock for UI tests.

--- briar-android/src/androidTestOfficial/java/org/briarproject/briar/android/BriarUiTestComponent.java
+++ briar-android/src/androidTestOfficial/java/org/briarproject/briar/android/BriarUiTestComponent.java
@@ -3,6 +3,7 @@ package org.briarproject.briar.android;
 import org.briarproject.bramble.BrambleAndroidModule;
 import org.briarproject.bramble.BrambleCoreModule;
 import org.briarproject.bramble.account.BriarAccountModule;
+import org.briarproject.bramble.system.ClockModule;
 import org.briarproject.briar.BriarCoreModule;
 import org.briarproject.briar.android.account.SignInTestCreateAccount;
 import org.briarproject.briar.android.account.SignInTestSignIn;
@@ -18,6 +19,7 @@ import dagger.Component;
 @Component(modules = {
 		AppModule.class,
 		AttachmentModule.class,
+		ClockModule.class,
 		MediaModule.class,
 		BriarCoreModule.class,
 		BrambleAndroidModule.class,
