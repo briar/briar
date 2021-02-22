ae41f98200c9937b1141a6ca099882d2d61cc944
Torsten Grote <t@grobox.de>
2021-02-22 09:57:18 -0300

    Add support for showing auto-delete timers in minutes

--- briar-android/src/test/java/org/briarproject/briar/android/util/UiUtilsFormatDurationTest.java
+++ briar-android/src/test/java/org/briarproject/briar/android/util/UiUtilsFormatDurationTest.java
@@ -12,12 +12,14 @@ import org.junit.Test;
 import static java.util.concurrent.TimeUnit.DAYS;
 import static java.util.concurrent.TimeUnit.HOURS;
 import static java.util.concurrent.TimeUnit.MINUTES;
+import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.briarproject.briar.android.util.UiUtils.formatDuration;
 
 public class UiUtilsFormatDurationTest extends BrambleMockTestCase {
 
 	private final Context ctx;
 	private final Resources r;
+	private final int strMinutes = R.plurals.duration_minutes;
 	private final int strHours = R.plurals.duration_hours;
 	private final int strDays = R.plurals.duration_days;
 
@@ -25,6 +27,12 @@ public class UiUtilsFormatDurationTest extends BrambleMockTestCase {
 		context.setImposteriser(ClassImposteriser.INSTANCE);
 		ctx = context.mock(Context.class);
 		r = context.mock(Resources.class);
+	}
+
+	@Test
+	public void testOneMinute() {
+		expectMinuteString(1);
+		formatDuration(ctx, MINUTES.toMillis(1));
 	}
 
 	@Test
@@ -40,9 +48,41 @@ public class UiUtilsFormatDurationTest extends BrambleMockTestCase {
 	}
 
 	@Test
+	public void test10Seconds() {
+		// capped to 1min
+		expectMinuteString(1);
+		formatDuration(ctx, SECONDS.toMillis(10));
+	}
+
+	@Test
+	public void test100Seconds() {
+		expectMinuteString(2);
+		formatDuration(ctx, SECONDS.toMillis(100));
+	}
+
+	@Test
+	public void test2Minutes() {
+		expectMinuteString(2);
+		formatDuration(ctx, MINUTES.toMillis(2));
+	}
+
+	@Test
 	public void test10Minutes() {
-		expectHourString(1);
+		expectMinuteString(10);
 		formatDuration(ctx, MINUTES.toMillis(10));
+	}
+
+	@Test
+	public void test130Minutes() {
+		expectHourString(2);
+		expectMinuteString(10);
+		formatDuration(ctx, MINUTES.toMillis(130));
+	}
+
+	@Test
+	public void test13Hours() {
+		expectHourString(13);
+		formatDuration(ctx, HOURS.toMillis(13));
 	}
 
 	@Test
@@ -67,7 +107,7 @@ public class UiUtilsFormatDurationTest extends BrambleMockTestCase {
 	@Test
 	public void testSevenDays40Minutes() {
 		expectDayString(7);
-		expectHourString(1);
+		expectMinuteString(40);
 		formatDuration(ctx, DAYS.toMillis(7) + MINUTES.toMillis(40));
 	}
 
@@ -90,6 +130,23 @@ public class UiUtilsFormatDurationTest extends BrambleMockTestCase {
 		expectDayString(2);
 		expectHourString(13);
 		formatDuration(ctx, DAYS.toMillis(2) + HOURS.toMillis(13));
+	}
+
+	@Test
+	public void test7Days23Hours55Minutes() {
+		expectDayString(7);
+		expectHourString(23);
+		expectMinuteString(55);
+		formatDuration(ctx,
+				DAYS.toMillis(7) + HOURS.toMillis(23) + MINUTES.toMillis(55));
+	}
+
+	private void expectMinuteString(int minutes) {
+		context.checking(new Expectations() {{
+			oneOf(ctx).getResources();
+			will(returnValue(r));
+			oneOf(r).getQuantityString(strMinutes, minutes, minutes);
+		}});
 	}
 
 	private void expectHourString(int hours) {
