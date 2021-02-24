292fb6d3b195034b791ad782677103fb496e609b
ameba23 <ameba23@systemli.org>
2021-02-24 12:39:51 +0100

    add combine shards stub

--- briar-core/src/main/java/org/briarproject/briar/socialbackup/DarkCrystal.java
+++ briar-core/src/main/java/org/briarproject/briar/socialbackup/DarkCrystal.java
@@ -4,10 +4,12 @@ import org.briarproject.bramble.api.crypto.SecretKey;
 import org.briarproject.bramble.api.nullsafety.NotNullByDefault;
 import org.briarproject.briar.api.socialbackup.Shard;
 
+import java.security.GeneralSecurityException;
 import java.util.List;
 
 @NotNullByDefault
 interface DarkCrystal {
 
 	List<Shard> createShards(SecretKey secret, int shards, int threshold);
+	SecretKey combineShards(List<Shard> shards) throws GeneralSecurityException;
 }
