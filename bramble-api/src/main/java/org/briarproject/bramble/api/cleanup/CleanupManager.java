7026361234fb5dfe4c7014bfe7293dbacf1db0c0
akwizgran <michael@briarproject.org>
2021-02-26 11:13:29 +0000

    Add javadocs for CleanupManager and CleanupHook.

--- bramble-api/src/main/java/org/briarproject/bramble/api/cleanup/CleanupManager.java
+++ bramble-api/src/main/java/org/briarproject/bramble/api/cleanup/CleanupManager.java
@@ -1,10 +1,27 @@
 package org.briarproject.bramble.api.cleanup;
 
+import org.briarproject.bramble.api.cleanup.event.CleanupTimerStartedEvent;
 import org.briarproject.bramble.api.crypto.SecretKey;
+import org.briarproject.bramble.api.db.DatabaseComponent;
+import org.briarproject.bramble.api.db.Transaction;
 import org.briarproject.bramble.api.lifecycle.LifecycleManager;
 import org.briarproject.bramble.api.nullsafety.NotNullByDefault;
 import org.briarproject.bramble.api.sync.ClientId;
+import org.briarproject.bramble.api.sync.MessageId;
 
+/**
+ * The CleanupManager is responsible for tracking the cleanup deadlines of
+ * messages and passing them to their respective
+ * {@link CleanupHook CleanupHooks} when the deadlines are reached.
+ * <p>
+ * The CleanupManager responds to
+ * {@link CleanupTimerStartedEvent CleanupTimerStartedEvents} broadcast by the
+ * {@link DatabaseComponent}.
+ * <p>
+ * See {@link DatabaseComponent#setCleanupTimerDuration(Transaction, MessageId, long)},
+ * {@link DatabaseComponent#startCleanupTimer(Transaction, MessageId)},
+ * {@link DatabaseComponent#stopCleanupTimer(Transaction, MessageId)}.
+ */
 @NotNullByDefault
 public interface CleanupManager {
 
