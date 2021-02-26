1162aa71ca9300a771f11cabc2dd8c1c378fcd14
akwizgran <michael@briarproject.org>
2021-02-26 11:13:29 +0000

    Add javadocs for CleanupManager and CleanupHook.

--- bramble-api/src/main/java/org/briarproject/bramble/api/cleanup/CleanupHook.java
+++ bramble-api/src/main/java/org/briarproject/bramble/api/cleanup/CleanupHook.java
@@ -1,14 +1,26 @@
 package org.briarproject.bramble.api.cleanup;
 
+import org.briarproject.bramble.api.cleanup.event.MessagesCleanedUpEvent;
 import org.briarproject.bramble.api.db.DbException;
 import org.briarproject.bramble.api.db.Transaction;
 import org.briarproject.bramble.api.nullsafety.NotNullByDefault;
 import org.briarproject.bramble.api.sync.GroupId;
 import org.briarproject.bramble.api.sync.MessageId;
 
+/**
+ * An interface for registering a hook with the {@link CleanupManager}
+ * that will be called when a message's cleanup deadline is reached.
+ */
 @NotNullByDefault
 public interface CleanupHook {
 
+	/**
+	 * Called when a message's cleanup deadline is reached. If this method
+	 * returns true, a {@link MessagesCleanedUpEvent} will be broadcast.
+	 *
+	 * @return True if the message has been deleted, or false if it has not,
+	 * in which case the message's cleanup timer will be stopped
+	 */
 	boolean deleteMessage(Transaction txn, GroupId g, MessageId m)
 			throws DbException;
 }
