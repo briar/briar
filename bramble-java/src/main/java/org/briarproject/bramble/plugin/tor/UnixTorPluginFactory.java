d7afbdf690af2964a3f443381be91af955a22eec
Nico Alt <nicoalt@posteo.org>
2021-03-09 12:00:00 +0000

    Use Tor binary for armhf (armv7)
    Example devices are Nexus 5 and Raspberry Pi v2.
    
    Based on https://code.briarproject.org/briar/briar/-/merge_requests/1376
    
    Related to https://code.briarproject.org/briar/briar/-/issues/1854

--- bramble-java/src/main/java/org/briarproject/bramble/plugin/tor/UnixTorPluginFactory.java
+++ bramble-java/src/main/java/org/briarproject/bramble/plugin/tor/UnixTorPluginFactory.java
@@ -25,6 +25,7 @@ import javax.annotation.concurrent.Immutable;
 import javax.inject.Inject;
 import javax.net.SocketFactory;
 
+import static java.util.logging.Level.INFO;
 import static java.util.logging.Logger.getLogger;
 import static org.briarproject.bramble.util.OsUtils.isLinux;
 
@@ -96,12 +97,15 @@ public class UnixTorPluginFactory implements DuplexPluginFactory {
 		String architecture = null;
 		if (isLinux()) {
 			String arch = System.getProperty("os.arch");
-			LOG.info("System's os.arch is " + arch);
+			if (LOG.isLoggable(INFO)) {
+				LOG.info("System's os.arch is " + arch);
+			}
 			if (arch.equals("amd64")) {
 				architecture = "linux-x86_64";
-			}
-			else if (arch.equals("aarch64")) {
+			} else if (arch.equals("aarch64")) {
 				architecture = "linux-aarch64";
+			} else if (arch.equals("arm")) {
+				architecture = "linux-armhf";
 			}
 		}
 		if (architecture == null) {
@@ -109,7 +113,9 @@ public class UnixTorPluginFactory implements DuplexPluginFactory {
 			return null;
 		}
 
-		LOG.info("The selected architecture for Tor is " + architecture);
+		if (LOG.isLoggable(INFO)) {
+			LOG.info("The selected architecture for Tor is " + architecture);
+		}
 
 		Backoff backoff = backoffFactory.createBackoff(MIN_POLLING_INTERVAL,
 				MAX_POLLING_INTERVAL, BACKOFF_BASE);
