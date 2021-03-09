28d2697e38a4904d5ffe8f0a26e20ac414b9fb64
ameba23 <ameba23@systemli.org>
2021-03-09 11:47:11 +0100

    add custodian names to Existing backup fragment

--- briar-android/src/main/java/org/briarproject/briar/android/socialbackup/ExistingBackupFragment.java
+++ briar-android/src/main/java/org/briarproject/briar/android/socialbackup/ExistingBackupFragment.java
@@ -7,10 +7,14 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import org.briarproject.bramble.api.identity.Author;
 import org.briarproject.briar.R;
 import org.briarproject.briar.android.activity.ActivityComponent;
 import org.briarproject.briar.android.fragment.BaseFragment;
 import org.briarproject.briar.api.socialbackup.BackupMetadata;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -18,6 +22,7 @@ import androidx.annotation.Nullable;
 public class ExistingBackupFragment extends BaseFragment {
 
 	private static final String THRESHOLD = "threshold";
+	private static final String CUSTODIANS = "custodians";
 	public static final String TAG = ExistingBackupFragment.class.getName();
     private TextView textViewBackupMetadata;
 
@@ -25,7 +30,12 @@ public class ExistingBackupFragment extends BaseFragment {
 	public static ExistingBackupFragment newInstance(
 			BackupMetadata backupMetadata) {
 		Bundle bundle = new Bundle();
-//		backupMetadata.getCustodians();
+		List<Author> custodians = backupMetadata.getCustodians();
+		ArrayList custodianNames = new ArrayList();
+	    for (Author custodian : custodians) {
+	    	custodianNames.add(custodian.getName());
+	    }
+	    bundle.putStringArrayList(CUSTODIANS, custodianNames);
 		bundle.putInt(THRESHOLD, backupMetadata.getThreshold());
 		ExistingBackupFragment fragment = new ExistingBackupFragment();
 		fragment.setArguments(bundle);
@@ -45,9 +55,13 @@ public class ExistingBackupFragment extends BaseFragment {
 		View view = inflater.inflate(R.layout.fragment_existing_backup,
 				container, false);
 		Bundle args = requireArguments();
-
+        ArrayList<String> custodianNames = args.getStringArrayList(CUSTODIANS);
+        String custodianNamesString = "";
+        for (String custodianName : custodianNames) {
+          	custodianNamesString += custodianName + " ";
+        }
         textViewBackupMetadata = view.findViewById(R.id.textViewBackupMetadata);
-        textViewBackupMetadata.setText(String.format("Threshold is %d", args.getInt(THRESHOLD)));
+        textViewBackupMetadata.setText(String.format("Threshold is %d", args.getInt(THRESHOLD)) + custodianNamesString);
 		return view;
 	}
 
