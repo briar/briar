d47c18b3926a94e7b6b04221bd43875ad3557312
ameba23 <ameba23@systemli.org>
2021-03-22 09:22:32 +0100

    ShardReceivedEvent in briar api

--- briar-api/src/main/java/org/briarproject/briar/api/socialbackup/ShardReceivedEvent.java
+++ briar-api/src/main/java/org/briarproject/briar/api/socialbackup/ShardReceivedEvent.java
@@ -1,4 +1,19 @@
 package org.briarproject.briar.api.socialbackup;
 
-public class ShardReceivedEvent {
+import org.briarproject.bramble.api.contact.ContactId;
+import org.briarproject.bramble.api.nullsafety.NotNullByDefault;
+import org.briarproject.briar.api.conversation.event.ConversationMessageReceivedEvent;
+
+import jdk.nashorn.internal.ir.annotations.Immutable;
+
+@Immutable
+@NotNullByDefault
+public class ShardReceivedEvent
+		extends ConversationMessageReceivedEvent<ShardMessageHeader> {
+
+	public ShardReceivedEvent(ShardMessageHeader shardMessageHeader,
+			ContactId contactId) {
+		super(shardMessageHeader, contactId);
+	}
+
 }
