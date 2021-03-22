2486a60feae4bb0714bf4b1accdf57b9eafc2898
ameba23 <ameba23@systemli.org>
2021-03-22 17:03:22 +0100

    only display custodian help recover explainer screen if you are a custodian

--- briar-android/src/main/java/org/briarproject/briar/android/socialbackup/CustodianHelpRecoverActivity.java
+++ briar-android/src/main/java/org/briarproject/briar/android/socialbackup/CustodianHelpRecoverActivity.java
@@ -1,11 +1,22 @@
 package org.briarproject.briar.android.socialbackup;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
+import org.briarproject.bramble.api.contact.ContactId;
+import org.briarproject.bramble.api.db.DatabaseComponent;
+import org.briarproject.bramble.api.db.DbException;
 import org.briarproject.briar.R;
 import org.briarproject.briar.android.activity.ActivityComponent;
 import org.briarproject.briar.android.activity.BriarActivity;
 import org.briarproject.briar.android.fragment.BaseFragment;
+import org.briarproject.briar.api.socialbackup.BackupMetadata;
+import org.briarproject.briar.api.socialbackup.SocialBackupManager;
+
+import javax.inject.Inject;
+
+import static org.briarproject.briar.android.conversation.ConversationActivity.CONTACT_ID;
 
 public class CustodianHelpRecoverActivity extends BriarActivity implements
 		BaseFragment.BaseFragmentListener, CustodianScanQrButtonListener {
@@ -14,15 +25,39 @@ public class CustodianHelpRecoverActivity extends BriarActivity implements
 		component.inject(this);
 	}
 
+	@Inject
+	public SocialBackupManager socialBackupManager;
+
+	@Inject
+	public DatabaseComponent db;
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_recover); // TODO change this
-        // TODO check if we have a shard for this secret owner
-		// if not we should not even display the menu item
-		CustodianRecoveryModeExplainerFragment fragment =
-				new CustodianRecoveryModeExplainerFragment();
-		showInitialFragment(fragment);
+
+		Intent intent = getIntent();
+		int id = intent.getIntExtra(CONTACT_ID, -1);
+		if (id == -1) throw new IllegalStateException("No ContactId");
+		ContactId contactId = new ContactId(id);
+
+        // check if we have a shard for this secret owner
+		try {
+			db.transaction(false, txn -> {
+				if (!socialBackupManager.amCustodian(txn, contactId)) {
+					throw new DbException();
+				}
+				CustodianRecoveryModeExplainerFragment fragment =
+						new CustodianRecoveryModeExplainerFragment();
+				showInitialFragment(fragment);
+			});
+		} catch (DbException e) {
+			// TODO improve this
+			Toast.makeText(this,
+					"You do not hold a backup shard from this contact",
+					Toast.LENGTH_SHORT).show();
+			finish();
+		}
 	}
 
 	@Override
