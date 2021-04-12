71c327112c78ae706f5cf56fd1dec32aee94dd78
ameba23 <ameba23@systemli.org>
2021-04-12 15:02:26 +0200

    Implement SecretOwnerTask

---
+++ briar-core/src/main/java/org/briarproject/briar/socialbackup/recovery/SecretOwnerTaskImpl.java
@@ -0,0 +1,39 @@
+package org.briarproject.briar.socialbackup.recovery;
+
+import org.briarproject.bramble.api.crypto.CryptoComponent;
+import org.briarproject.bramble.api.crypto.KeyPair;
+import org.briarproject.bramble.api.lifecycle.IoExecutor;
+import org.briarproject.briar.api.socialbackup.recovery.SecretOwnerTask;
+
+import java.net.InetSocketAddress;
+import java.util.concurrent.Executor;
+
+import javax.inject.Inject;
+
+public class SecretOwnerTaskImpl implements SecretOwnerTask {
+
+	private final CryptoComponent crypto;
+	private final Executor ioExecutor;
+	private final KeyPair localKeyPair;
+	private boolean cancelled = false;
+
+	@Inject
+	SecretOwnerTaskImpl(CryptoComponent crypto,
+		@IoExecutor Executor ioExecutor) {
+		this.crypto = crypto;
+		this.ioExecutor = ioExecutor;
+		localKeyPair = crypto.generateAgreementKeyPair();
+	}
+
+	@Override
+	public void start(Observer observer) {
+		// TODO use the actual ip address on local network
+		InetSocketAddress socketAddress = InetSocketAddress.createUnresolved("localhost", 1234);
+		observer.onStateChanged(new State.Listening(localKeyPair.getPublic(), socketAddress));
+	}
+
+	@Override
+	public void cancel() {
+        cancelled = true;
+	}
+}
