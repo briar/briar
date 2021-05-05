c5d6ee6782344ae0bd485b4f0bffd36ccfaaa0ff
akwizgran <michael@briarproject.org>
2021-05-05 17:52:37 +0100

    Add DB method for getting amount of data to sync.

--- bramble-core/src/main/java/org/briarproject/bramble/db/Database.java
+++ bramble-core/src/main/java/org/briarproject/bramble/db/Database.java
@@ -348,6 +348,16 @@ interface Database<T> {
 	Message getMessage(T txn, MessageId m) throws DbException;
 
 	/**
+	 * Returns the total length, including headers, of any messages that are
+	 * eligible to be sent to the given contact via a transport with the given
+	 * max latency.
+	 * <p/>
+	 * Read-only.
+	 */
+	long getMessageBytesToSend(T txn, ContactId c, int maxLatency)
+			throws DbException;
+
+	/**
 	 * Returns the IDs and states of all dependencies of the given message.
 	 * For missing dependencies and dependencies in other groups, the state
 	 * {@link MessageState UNKNOWN} is returned.
