b5472f55576613df293237d084901fcdad200c4e
Torsten Grote <t@grobox.de>
2021-05-06 17:34:40 -0300

    Implement intro screen for offline app sharing

--- briar-android/src/main/java/org/briarproject/briar/android/AndroidComponent.java
+++ briar-android/src/main/java/org/briarproject/briar/android/AndroidComponent.java
@@ -35,6 +35,7 @@ import org.briarproject.briar.BriarCoreModule;
 import org.briarproject.briar.android.attachment.AttachmentModule;
 import org.briarproject.briar.android.attachment.media.MediaModule;
 import org.briarproject.briar.android.conversation.glide.BriarModelLoader;
+import org.briarproject.briar.android.hotspot.HotspotIntroFragment;
 import org.briarproject.briar.android.logging.CachingLogHandler;
 import org.briarproject.briar.android.login.SignInReminderReceiver;
 import org.briarproject.briar.android.settings.ConnectionsFragment;
@@ -210,4 +211,6 @@ public interface AndroidComponent
 	void inject(SecurityFragment securityFragment);
 
 	void inject(NotificationsFragment notificationsFragment);
+
+	void inject(HotspotIntroFragment hotspotIntroFragment);
 }
---
+++ briar-android/src/main/java/org/briarproject/briar/android/hotspot/HotspotViewModel.java
@@ -0,0 +1,28 @@
+package org.briarproject.briar.android.hotspot;
+
+import android.app.Application;
+
+import org.briarproject.bramble.api.db.DatabaseExecutor;
+import org.briarproject.bramble.api.db.TransactionManager;
+import org.briarproject.bramble.api.lifecycle.LifecycleManager;
+import org.briarproject.bramble.api.nullsafety.NotNullByDefault;
+import org.briarproject.bramble.api.system.AndroidExecutor;
+import org.briarproject.briar.android.viewmodel.DbViewModel;
+
+import java.util.concurrent.Executor;
+
+import javax.inject.Inject;
+
+@NotNullByDefault
+class HotspotViewModel extends DbViewModel {
+
+	@Inject
+	HotspotViewModel(Application application,
+			@DatabaseExecutor Executor dbExecutor,
+			LifecycleManager lifecycleManager,
+			TransactionManager db,
+			AndroidExecutor androidExecutor) {
+		super(application, dbExecutor, lifecycleManager, db, androidExecutor);
+	}
+
+}
