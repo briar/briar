a86ba50dec33f96dfe7f28a0b5cac53ace41cb82
Torsten Grote <t@grobox.de>
2021-05-06 17:34:40 -0300

    Implement intro screen for offline app sharing

---
+++ briar-android/src/main/java/org/briarproject/briar/android/hotspot/HotspotActivity.java
@@ -0,0 +1,62 @@
+package org.briarproject.briar.android.hotspot;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import org.briarproject.bramble.api.nullsafety.MethodsNotNullByDefault;
+import org.briarproject.bramble.api.nullsafety.ParametersNotNullByDefault;
+import org.briarproject.briar.R;
+import org.briarproject.briar.android.activity.ActivityComponent;
+import org.briarproject.briar.android.activity.BriarActivity;
+
+import javax.annotation.Nullable;
+import javax.inject.Inject;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.lifecycle.ViewModelProvider;
+
+@MethodsNotNullByDefault
+@ParametersNotNullByDefault
+public class HotspotActivity extends BriarActivity {
+
+	@Inject
+	ViewModelProvider.Factory viewModelFactory;
+
+	private HotspotViewModel viewModel;
+
+	@Override
+	public void injectActivity(ActivityComponent component) {
+		component.inject(this);
+		viewModel = new ViewModelProvider(this, viewModelFactory)
+				.get(HotspotViewModel.class);
+	}
+
+	@Override
+	public void onCreate(@Nullable Bundle state) {
+		super.onCreate(state);
+		setContentView(R.layout.activity_fragment_container);
+
+		ActionBar ab = getSupportActionBar();
+		if (ab != null) {
+			ab.setDisplayHomeAsUpEnabled(true);
+		}
+
+		if (state == null) {
+			getSupportFragmentManager().beginTransaction()
+					.replace(R.id.fragmentContainer, new HotspotIntroFragment(),
+							HotspotIntroFragment.TAG)
+					.commit();
+		}
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+		if (item.getItemId() == android.R.id.home) {
+			onBackPressed();
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+}
---
+++ briar-android/src/main/java/org/briarproject/briar/android/hotspot/HotspotIntroFragment.java
@@ -0,0 +1,67 @@
+package org.briarproject.briar.android.hotspot;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import org.briarproject.bramble.api.nullsafety.MethodsNotNullByDefault;
+import org.briarproject.bramble.api.nullsafety.ParametersNotNullByDefault;
+import org.briarproject.briar.R;
+
+import javax.inject.Inject;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import static android.view.View.INVISIBLE;
+import static android.view.View.VISIBLE;
+import static androidx.transition.TransitionManager.beginDelayedTransition;
+import static org.briarproject.briar.android.AppModule.getAndroidComponent;
+
+@MethodsNotNullByDefault
+@ParametersNotNullByDefault
+public class HotspotIntroFragment extends Fragment {
+
+	public final static String TAG = HotspotIntroFragment.class.getName();
+
+	@Inject
+	ViewModelProvider.Factory viewModelFactory;
+
+	private HotspotViewModel viewModel;
+
+	@Override
+	public void onAttach(Context context) {
+		super.onAttach(context);
+		getAndroidComponent(requireContext()).inject(this);
+		viewModel = new ViewModelProvider(requireActivity(), viewModelFactory)
+				.get(HotspotViewModel.class);
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater,
+			@Nullable ViewGroup container,
+			@Nullable Bundle savedInstanceState) {
+		View v = inflater
+				.inflate(R.layout.fragment_hotspot_intro, container, false);
+
+		Button startButton = v.findViewById(R.id.startButton);
+		ProgressBar progressBar = v.findViewById(R.id.progressBar);
+		TextView progressTextView = v.findViewById(R.id.progressTextView);
+
+		startButton.setOnClickListener(button -> {
+			beginDelayedTransition((ViewGroup) v);
+			startButton.setVisibility(INVISIBLE);
+			progressBar.setVisibility(VISIBLE);
+			progressTextView.setVisibility(VISIBLE);
+		});
+
+		return v;
+	}
+
+}
