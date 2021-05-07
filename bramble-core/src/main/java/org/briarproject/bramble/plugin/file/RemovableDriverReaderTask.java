f0687a082af373eacbd0e73e62d3c476091f8eec
akwizgran <michael@briarproject.org>
2021-05-07 14:20:30 +0100

    Implement RemovableDriverReaderTask.

--- bramble-core/src/main/java/org/briarproject/bramble/plugin/file/RemovableDriverReaderTask.java
+++ bramble-core/src/main/java/org/briarproject/bramble/plugin/file/RemovableDriverReaderTask.java
@@ -1,46 +1,100 @@
 package org.briarproject.bramble.plugin.file;
 
+import org.briarproject.bramble.api.connection.ConnectionManager;
 import org.briarproject.bramble.api.contact.ContactId;
+import org.briarproject.bramble.api.event.Event;
+import org.briarproject.bramble.api.event.EventBus;
+import org.briarproject.bramble.api.event.EventListener;
 import org.briarproject.bramble.api.nullsafety.NotNullByDefault;
+import org.briarproject.bramble.api.plugin.PluginManager;
+import org.briarproject.bramble.api.plugin.TransportConnectionReader;
+import org.briarproject.bramble.api.sync.event.MessageAddedEvent;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.concurrent.Executor;
+import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
-import static java.util.logging.Level.WARNING;
+import static java.lang.Math.min;
 import static java.util.logging.Logger.getLogger;
-import static org.briarproject.bramble.util.IoUtils.tryToClose;
-import static org.briarproject.bramble.util.LogUtils.logException;
+import static org.briarproject.bramble.api.plugin.file.RemovableDriveConstants.ID;
 
 @NotNullByDefault
-class RemovableDriverReaderTask extends RemovableDriveTaskImpl {
+class RemovableDriverReaderTask extends RemovableDriveTaskImpl
+		implements EventListener {
 
 	private final static Logger LOG =
 			getLogger(RemovableDriverReaderTask.class.getName());
 
-	RemovableDriverReaderTask(Executor eventExecutor,
-			RemovableDriveTaskRegistry registry, ContactId contactId,
+	private final AtomicLong fileLength = new AtomicLong(0);
+	private final AtomicLong totalMessageLength = new AtomicLong(0);
+
+	RemovableDriverReaderTask(
+			Executor eventExecutor,
+			PluginManager pluginManager,
+			ConnectionManager connectionManager,
+			EventBus eventBus,
+			RemovableDriveTaskRegistry registry,
+			ContactId contactId,
 			File file) {
-		super(eventExecutor, registry, contactId, file);
+		super(eventExecutor, pluginManager, connectionManager, eventBus,
+				registry, contactId, file);
 	}
 
 	@Override
 	public void run() {
-		// TODO
-		InputStream in = null;
-		try {
-			visitObservers(o -> o.onProgress(0, 100));
-			in = new FileInputStream(file);
-			visitObservers(o -> o.onCompletion(true));
-		} catch (IOException e) {
-			logException(LOG, WARNING, e);
+		TransportConnectionReader r =
+				getPlugin().createReader(createProperties());
+		if (r == null) {
+			LOG.warning("Failed to create reader");
+			registry.removeReader(contactId, RemovableDriverReaderTask.this);
 			visitObservers(o -> o.onCompletion(false));
-		} finally {
-			tryToClose(in, LOG, WARNING);
-			registry.removeReader(contactId, this);
+			return;
+		}
+		fileLength.set(file.length());
+		eventBus.addListener(this);
+		connectionManager.manageIncomingConnection(ID, new DecoratedReader(r));
+	}
+
+	@Override
+	public void eventOccurred(Event e) {
+		if (e instanceof MessageAddedEvent) {
+			MessageAddedEvent m = (MessageAddedEvent) e;
+			if (contactId.equals(m.getContactId())) {
+				LOG.info("Message received");
+				updateProgress(m.getMessage().getRawLength());
+			}
+		}
+	}
+
+	private void updateProgress(int messageLength) {
+		long done = totalMessageLength.addAndGet(messageLength);
+		long total = fileLength.get();
+		visitObservers(o -> o.onProgress(min(done, total), total));
+	}
+
+	private class DecoratedReader implements TransportConnectionReader {
+
+		private final TransportConnectionReader delegate;
+
+		private DecoratedReader(TransportConnectionReader delegate) {
+			this.delegate = delegate;
+		}
+
+		@Override
+		public InputStream getInputStream() throws IOException {
+			return delegate.getInputStream();
+		}
+
+		@Override
+		public void dispose(boolean exception, boolean recognised)
+				throws IOException {
+			delegate.dispose(exception, recognised);
+			registry.removeReader(contactId, RemovableDriverReaderTask.this);
+			eventBus.removeListener(RemovableDriverReaderTask.this);
+			visitObservers(o -> o.onCompletion(!exception && recognised));
 		}
 	}
 }
