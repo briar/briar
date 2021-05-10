c8c28eb6a003e7394db163be61420d9b96e6b8eb
ameba23 <ameba23@systemli.org>
2021-05-10 10:31:04 +0200

    Add method to get the local list of wipers

--- briar-api/src/main/java/org/briarproject/briar/api/remotewipe/RemoteWipeManager.java
+++ briar-api/src/main/java/org/briarproject/briar/api/remotewipe/RemoteWipeManager.java
@@ -5,6 +5,7 @@ import org.briarproject.bramble.api.contact.Contact;
 import org.briarproject.bramble.api.contact.ContactId;
 import org.briarproject.bramble.api.db.DbException;
 import org.briarproject.bramble.api.db.Transaction;
+import org.briarproject.bramble.api.identity.Author;
 import org.briarproject.bramble.api.nullsafety.NotNullByDefault;
 import org.briarproject.bramble.api.sync.ClientId;
 import org.briarproject.briar.api.conversation.ConversationManager;
@@ -45,6 +46,10 @@ public interface RemoteWipeManager extends ConversationManager.ConversationClien
 			throws DbException, FormatException;
 
 	boolean amWiper(Transaction txn, ContactId contactId);
+
+	boolean remoteWipeIsSetup(Transaction txn);
+
+	List<Author> getWipers(Transaction txn) throws DbException;
 
 	@Override
 	Collection<ConversationMessageHeader> getMessageHeaders(
