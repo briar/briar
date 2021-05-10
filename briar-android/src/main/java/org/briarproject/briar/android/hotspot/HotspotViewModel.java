1b7007d4ef0101cd314b5b3b363905123adb565a
Torsten Grote <t@grobox.de>
2021-05-10 14:41:37 -0300

    Show notification while hotspot is active

--- briar-android/src/main/java/org/briarproject/briar/android/hotspot/HotspotViewModel.java
+++ briar-android/src/main/java/org/briarproject/briar/android/hotspot/HotspotViewModel.java
@@ -8,6 +8,7 @@ import org.briarproject.bramble.api.lifecycle.LifecycleManager;
 import org.briarproject.bramble.api.nullsafety.NotNullByDefault;
 import org.briarproject.bramble.api.system.AndroidExecutor;
 import org.briarproject.briar.android.viewmodel.DbViewModel;
+import org.briarproject.briar.api.android.AndroidNotificationManager;
 
 import java.util.concurrent.Executor;
 
@@ -16,13 +17,31 @@ import javax.inject.Inject;
 @NotNullByDefault
 class HotspotViewModel extends DbViewModel {
 
+	private final AndroidNotificationManager notificationManager;
+
 	@Inject
 	HotspotViewModel(Application application,
 			@DatabaseExecutor Executor dbExecutor,
 			LifecycleManager lifecycleManager,
 			TransactionManager db,
-			AndroidExecutor androidExecutor) {
+			AndroidExecutor androidExecutor,
+			AndroidNotificationManager notificationManager) {
 		super(application, dbExecutor, lifecycleManager, db, androidExecutor);
+		this.notificationManager = notificationManager;
+	}
+
+	void startHotspot() {
+		notificationManager.showHotspotNotification();
+	}
+
+	private void stopHotspot() {
+		notificationManager.clearHotspotNotification();
+	}
+
+	@Override
+	protected void onCleared() {
+		super.onCleared();
+		stopHotspot();
 	}
 
 	// TODO copy actual code from Offline Hotspot app
