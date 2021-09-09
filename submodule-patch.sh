c9642be4caff0ae32abdcd56c391a36ea42a074e
ialokim <ialokim@mailbox.org>
2021-09-09 13:20:34 +0200

    add instructions for patch

--- submodule-patch.sh
+++ submodule-patch.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# patch required to use briar as a gradle subproject (e.g. in brair-desktop):
+# 1. git merge <latest-release-tag>
+# 2. git checkout --theirs .
+# 3. git add .
+# 4. git merge --continue
+# 5. ./submodule-patch.sh
+# 6. git add .
+# 7. git commit -m "apply patch"
+
 sed -i "s/':bramble-/':briar:bramble-/g" bramble-{android,core,java}/build.gradle
 sed -i "s/':bramble-/':briar:bramble-/g" briar-{android,core,api}/build.gradle
-sed -i "s/':briar-/':briar:bria-/g" briar-{android,core,api}/build.gradle
+sed -i "s/':briar-/':briar:briar-/g" briar-{android,core,api}/build.gradle
