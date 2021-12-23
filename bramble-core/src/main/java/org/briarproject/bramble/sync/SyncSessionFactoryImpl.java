53f229b1161790a7a6b60386fc3d85a206c14158
akwizgran <michael@briarproject.org>
2021-12-23 11:46:27 +0000

    Record IDs if latency is high and not using eager retransmission.

--- bramble-core/src/main/java/org/briarproject/bramble/sync/SyncSessionFactoryImpl.java
+++ bramble-core/src/main/java/org/briarproject/bramble/sync/SyncSessionFactoryImpl.java
@@ -1,6 +1,7 @@
 package org.briarproject.bramble.sync;
 
 import org.briarproject.bramble.api.contact.ContactId;
+import org.briarproject.bramble.api.crypto.CryptoComponent;
 import org.briarproject.bramble.api.db.DatabaseComponent;
 import org.briarproject.bramble.api.db.DatabaseExecutor;
 import org.briarproject.bramble.api.event.EventBus;
@@ -14,6 +15,7 @@ import org.briarproject.bramble.api.sync.SyncRecordWriter;
 import org.briarproject.bramble.api.sync.SyncRecordWriterFactory;
 import org.briarproject.bramble.api.sync.SyncSession;
 import org.briarproject.bramble.api.sync.SyncSessionFactory;
+import org.briarproject.bramble.api.sync.SyncSessionId;
 import org.briarproject.bramble.api.system.Clock;
 import org.briarproject.bramble.api.transport.StreamWriter;
 
@@ -25,12 +27,17 @@ import javax.annotation.Nullable;
 import javax.annotation.concurrent.Immutable;
 import javax.inject.Inject;
 
+import static java.util.concurrent.TimeUnit.MINUTES;
+
 @Immutable
 @NotNullByDefault
 class SyncSessionFactoryImpl implements SyncSessionFactory {
 
+	private static final long ONE_MINUTE = MINUTES.toMillis(1);
+
 	private final DatabaseComponent db;
 	private final Executor dbExecutor;
+	private final CryptoComponent crypto;
 	private final EventBus eventBus;
 	private final Clock clock;
 	private final SyncRecordReaderFactory recordReaderFactory;
@@ -38,11 +45,15 @@ class SyncSessionFactoryImpl implements SyncSessionFactory {
 
 	@Inject
 	SyncSessionFactoryImpl(DatabaseComponent db,
-			@DatabaseExecutor Executor dbExecutor, EventBus eventBus,
-			Clock clock, SyncRecordReaderFactory recordReaderFactory,
+			@DatabaseExecutor Executor dbExecutor,
+			CryptoComponent crypto,
+			EventBus eventBus,
+			Clock clock,
+			SyncRecordReaderFactory recordReaderFactory,
 			SyncRecordWriterFactory recordWriterFactory) {
 		this.db = db;
 		this.dbExecutor = dbExecutor;
+		this.crypto = crypto;
 		this.eventBus = eventBus;
 		this.clock = clock;
 		this.recordReaderFactory = recordReaderFactory;
@@ -61,11 +72,19 @@ class SyncSessionFactoryImpl implements SyncSessionFactory {
 	@Override
 	public SyncSession createSimplexOutgoingSession(ContactId c, TransportId t,
 			long maxLatency, boolean eager, StreamWriter streamWriter) {
+		// If we're not using eager retransmission and the max latency is more
+		// than 1 minute, record the IDs of sent and acked messages so we can
+		// resend them without a long delay if we crash mid-session
+		SyncSessionId syncSessionId = null;
+		if (!eager && maxLatency > ONE_MINUTE) {
+			syncSessionId =
+					new SyncSessionId(crypto.generateUniqueId().getBytes());
+		}
 		OutputStream out = streamWriter.getOutputStream();
 		SyncRecordWriter recordWriter =
 				recordWriterFactory.createRecordWriter(out);
 		return new SimplexOutgoingSession(db, eventBus, c, t,
-				maxLatency, eager, streamWriter, recordWriter, null);
+				maxLatency, eager, streamWriter, recordWriter, syncSessionId);
 	}
 
 	@Override
