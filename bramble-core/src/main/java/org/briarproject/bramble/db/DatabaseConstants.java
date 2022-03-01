97f64fb31cbfd179f4f565d48f94eac590bc2dca
akwizgran <michael@briarproject.org>
2022-03-01 12:44:15 +0000

    Remove the max compaction interval.
    We always compact on shutdown, or on startup if we've shut down uncleanly without compacting.

--- bramble-core/src/main/java/org/briarproject/bramble/db/DatabaseConstants.java
+++ bramble-core/src/main/java/org/briarproject/bramble/db/DatabaseConstants.java
@@ -2,8 +2,6 @@ package org.briarproject.bramble.db;
 
 import org.briarproject.bramble.api.settings.Settings;
 
-import static java.util.concurrent.TimeUnit.DAYS;
-
 interface DatabaseConstants {
 
 	/**
@@ -24,19 +22,6 @@ interface DatabaseConstants {
 	 * stored.
 	 */
 	String SCHEMA_VERSION_KEY = "schemaVersion";
-
-	/**
-	 * The {@link Settings} key under which the time of the last database
-	 * compaction is stored.
-	 */
-	String LAST_COMPACTED_KEY = "lastCompacted";
-
-	/**
-	 * The maximum time between database compactions in milliseconds. When the
-	 * database is opened it will be compacted if more than this amount of time
-	 * has passed since the last compaction.
-	 */
-	long MAX_COMPACTION_INTERVAL_MS = DAYS.toMillis(30);
 
 	/**
 	 * The {@link Settings} key under which the flag is stored indicating
