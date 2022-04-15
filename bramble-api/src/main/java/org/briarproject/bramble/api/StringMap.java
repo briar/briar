16ab48d0092982c02097e7027b0626538c2e7520
Daniel Lublin <daniel@lublin.se>
2022-04-15 10:24:37 +0200

    Allow storing int array in settings

--- bramble-api/src/main/java/org/briarproject/bramble/api/StringMap.java
+++ bramble-api/src/main/java/org/briarproject/bramble/api/StringMap.java
@@ -1,7 +1,13 @@
 package org.briarproject.bramble.api;
 
+import org.briarproject.bramble.util.StringUtils;
+
+import java.util.ArrayList;
 import java.util.Hashtable;
+import java.util.List;
 import java.util.Map;
+
+import javax.annotation.Nullable;
 
 public abstract class StringMap extends Hashtable<String, String> {
 
@@ -51,5 +57,32 @@ public abstract class StringMap extends Hashtable<String, String> {
 
 	public void putLong(String key, long value) {
 		put(key, String.valueOf(value));
+	}
+
+	@Nullable
+	public int[] getIntArray(String key) {
+		String s = get(key);
+		if (s == null) return null;
+		// Handle empty string because "".split(",") returns {""}
+		if (s.length() == 0) return new int[0];
+		String[] intStrings = s.split(",");
+		int[] ints = new int[intStrings.length];
+		try {
+			for (int i = 0; i < ints.length; i++) {
+				ints[i] = Integer.parseInt(intStrings[i]);
+			}
+		} catch (NumberFormatException e) {
+			return null;
+		}
+		return ints;
+	}
+
+	public void putIntArray(String key, int[] value) {
+		List<String> intStrings = new ArrayList<>();
+		for (int integer : value) {
+			intStrings.add(String.valueOf(integer));
+		}
+		// Puts empty string if input array value is empty
+		put(key, StringUtils.join(intStrings, ","));
 	}
 }
