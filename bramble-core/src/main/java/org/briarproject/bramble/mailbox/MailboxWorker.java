79f41064e4850fefe34e1f1a71c2e8e53fb2e4e9
akwizgran <michael@briarproject.org>
2022-06-02 14:25:05 +0100

    Add download worker for a contact's mailbox.

---
+++ bramble-core/src/main/java/org/briarproject/bramble/mailbox/MailboxWorker.java
@@ -0,0 +1,23 @@
+package org.briarproject.bramble.mailbox;
+
+import org.briarproject.bramble.api.nullsafety.NotNullByDefault;
+
+import javax.annotation.concurrent.ThreadSafe;
+
+/**
+ * A worker that downloads files from a contact's mailbox.
+ */
+@ThreadSafe
+@NotNullByDefault
+interface MailboxWorker {
+
+	/**
+	 * Asynchronously starts the worker.
+	 */
+	void start();
+
+	/**
+	 * Destroys the worker and cancels any pending tasks or retries.
+	 */
+	void destroy();
+}
