ddc6606ccfa76a47bf15b843be979d47b0f46775
FlyingP1g FlyingP1g <rukasmail@protonmail.com>
2022-07-01 17:25:14 +0300

    About menu improvements.

--- briar-android/src/main/java/org/briarproject/briar/android/settings/AboutFragment.java
+++ briar-android/src/main/java/org/briarproject/briar/android/settings/AboutFragment.java
@@ -1,11 +1,14 @@
 package org.briarproject.briar.android.settings;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.text.method.LinkMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.briarproject.bramble.api.nullsafety.MethodsNotNullByDefault;
 import org.briarproject.bramble.api.nullsafety.ParametersNotNullByDefault;
@@ -13,13 +16,23 @@ import org.briarproject.briar.BuildConfig;
 import org.briarproject.briar.R;
 
 
+import java.util.logging.Logger;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import static android.widget.Toast.LENGTH_LONG;
+import static java.util.logging.Level.WARNING;
+import static java.util.logging.Logger.getLogger;
+import static org.briarproject.bramble.util.LogUtils.logException;
+
 @MethodsNotNullByDefault
 @ParametersNotNullByDefault
 public class AboutFragment extends Fragment {
+
+	final static String TAG = AboutFragment.class.getName();
+	private static final Logger LOG = getLogger(TAG);
 
 	private TextView briarVersion;
 	private TextView briarWebsite;
@@ -39,12 +52,34 @@ public class AboutFragment extends Fragment {
 		super.onStart();
 		requireActivity().setTitle(R.string.about_title);
 		briarVersion = requireActivity().findViewById(R.id.BriarVersion);
-		briarVersion.setText(getString(R.string.briar_version) + " " +
-				BuildConfig.VERSION_NAME);
+		briarVersion.setText(
+				getString(R.string.briar_version, BuildConfig.VERSION_NAME));
 		briarWebsite = requireActivity().findViewById(R.id.BriarWebsite);
 		briarSourceCode = requireActivity().findViewById(R.id.BriarSourceCode);
-		briarWebsite.setMovementMethod(LinkMovementMethod.getInstance());
-		briarSourceCode.setMovementMethod(LinkMovementMethod.getInstance());
+		briarWebsite.setOnClickListener(View -> {
+			String url = "https://briarproject.org/";
+			Intent i = new Intent(Intent.ACTION_VIEW);
+			i.setData(Uri.parse(url));
+			try {
+				startActivity(i);
+			} catch (ActivityNotFoundException e) {
+				logException(LOG, WARNING, e);
+				Toast.makeText(requireContext(),
+						R.string.error_start_activity, LENGTH_LONG).show();
+			}
+		});
+		briarSourceCode.setOnClickListener(View -> {
+			String url = "https://code.briarproject.org/briar/briar";
+			Intent i = new Intent(Intent.ACTION_VIEW);
+			i.setData(Uri.parse(url));
+			try {
+				startActivity(i);
+			} catch (ActivityNotFoundException e) {
+				logException(LOG, WARNING, e);
+				Toast.makeText(requireContext(),
+						R.string.error_start_activity, LENGTH_LONG).show();
+			}
+		});
 	}
 
 }
