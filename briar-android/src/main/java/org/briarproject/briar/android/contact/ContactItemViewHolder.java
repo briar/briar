7a888a6114403c5f7b9f4143fd8ae55d98db6e59
johndoe4221 <lkdeatour-briarproject@yahoo.com>
2022-07-14 19:23:07 +0200

    feat: [1052] show trust-indicator in contact list

--- briar-android/src/main/java/org/briarproject/briar/android/contact/ContactItemViewHolder.java
+++ briar-android/src/main/java/org/briarproject/briar/android/contact/ContactItemViewHolder.java
@@ -7,6 +7,8 @@ import android.widget.TextView;
 
 import org.briarproject.bramble.api.nullsafety.NotNullByDefault;
 import org.briarproject.briar.R;
+import org.briarproject.briar.android.view.TrustIndicatorView;
+import org.briarproject.briar.api.identity.AuthorInfo;
 
 import javax.annotation.Nullable;
 
@@ -26,6 +28,10 @@ public class ContactItemViewHolder<I extends ContactItem>
 	protected final TextView name;
 	@Nullable
 	protected final ImageView bulb;
+	@Nullable
+	protected final TrustIndicatorView trustIndicator;
+	@Nullable
+	protected final TextView trustIndicatorDescription;
 
 	public ContactItemViewHolder(View v) {
 		super(v);
@@ -35,6 +41,11 @@ public class ContactItemViewHolder<I extends ContactItem>
 		name = v.findViewById(R.id.nameView);
 		// this can be null as not all layouts that use this ViewHolder have it
 		bulb = v.findViewById(R.id.bulbView);
+		// this can be null as not all layouts that use this ViewHolder have it
+		trustIndicator = v.findViewById(R.id.trustIndicator);
+		// this can be null as not all layouts that use this ViewHolder have it
+		trustIndicatorDescription =
+				v.findViewById(R.id.trustIndicatorDescription);
 	}
 
 	protected void bind(I item, @Nullable OnContactClickListener<I> listener) {
@@ -47,6 +58,29 @@ public class ContactItemViewHolder<I extends ContactItem>
 				bulb.setImageResource(R.drawable.contact_connected);
 			} else {
 				bulb.setImageResource(R.drawable.contact_disconnected);
+			}
+		}
+
+		if (trustIndicator != null && trustIndicatorDescription != null) {
+			final AuthorInfo.Status status = item.getAuthorInfo().getStatus();
+			trustIndicator.setTrustLevel(status);
+
+			switch (status) {
+				case UNVERIFIED:
+					trustIndicatorDescription.setText(
+							R.string.peer_trust_level_unverified);
+					break;
+				case VERIFIED:
+					trustIndicatorDescription.setText(
+							R.string.peer_trust_level_verified);
+					break;
+				case OURSELVES:
+					trustIndicatorDescription.setText(
+							R.string.peer_trust_level_ourselves);
+					break;
+				default:
+					trustIndicatorDescription.setText(
+							R.string.peer_trust_level_stranger);
 			}
 		}
 
