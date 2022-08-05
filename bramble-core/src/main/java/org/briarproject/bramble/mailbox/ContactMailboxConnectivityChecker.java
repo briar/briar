dfcd6260817a17881bc6f747678b6b59c49aa6c9
akwizgran <michael@briarproject.org>
2022-08-05 13:35:59 +0100

    Add some logging to connectivity checks.

--- bramble-core/src/main/java/org/briarproject/bramble/mailbox/ContactMailboxConnectivityChecker.java
+++ bramble-core/src/main/java/org/briarproject/bramble/mailbox/ContactMailboxConnectivityChecker.java
@@ -5,12 +5,19 @@ import org.briarproject.bramble.api.nullsafety.NotNullByDefault;
 import org.briarproject.bramble.api.system.Clock;
 import org.briarproject.bramble.mailbox.MailboxApi.ApiException;
 
+import java.util.logging.Logger;
+
 import javax.annotation.concurrent.ThreadSafe;
 import javax.inject.Inject;
+
+import static java.util.logging.Logger.getLogger;
 
 @ThreadSafe
 @NotNullByDefault
 class ContactMailboxConnectivityChecker extends ConnectivityCheckerImpl {
+
+	private static final Logger LOG =
+			getLogger(ContactMailboxConnectivityChecker.class.getName());
 
 	private final MailboxApi mailboxApi;
 
@@ -25,7 +32,9 @@ class ContactMailboxConnectivityChecker extends ConnectivityCheckerImpl {
 	ApiCall createConnectivityCheckTask(MailboxProperties properties) {
 		if (properties.isOwner()) throw new IllegalArgumentException();
 		return new SimpleApiCall(() -> {
+			LOG.info("Checking connectivity of contact's mailbox");
 			if (!mailboxApi.checkStatus(properties)) throw new ApiException();
+			LOG.info("Contact's mailbox is reachable");
 			// Call the observers and cache the result
 			onConnectivityCheckSucceeded(clock.currentTimeMillis());
 		});
