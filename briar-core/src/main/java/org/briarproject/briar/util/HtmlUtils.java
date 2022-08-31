85361b0099390b950de8fcd59e4e1a5c8ff73992
akwizgran <michael@briarproject.org>
2022-08-31 12:32:08 +0100

    Upgrade jsoup to 1.15.3.

--- briar-core/src/main/java/org/briarproject/briar/util/HtmlUtils.java
+++ briar-core/src/main/java/org/briarproject/briar/util/HtmlUtils.java
@@ -2,16 +2,16 @@ package org.briarproject.briar.util;
 
 import org.briarproject.bramble.api.nullsafety.NotNullByDefault;
 import org.jsoup.Jsoup;
-import org.jsoup.safety.Whitelist;
+import org.jsoup.safety.Safelist;
 
 @NotNullByDefault
 public class HtmlUtils {
 
-	public static Whitelist STRIP_ALL = Whitelist.none();
-	public static Whitelist ARTICLE =
-			Whitelist.basic().addTags("h1", "h2", "h3", "h4", "h5", "h6");
+	public static Safelist STRIP_ALL = Safelist.none();
+	public static Safelist ARTICLE = Safelist.basic()
+			.addTags("h1", "h2", "h3", "h4", "h5", "h6");
 
-	public static String clean(String s, Whitelist list) {
+	public static String clean(String s, Safelist list) {
 		return Jsoup.clean(s, list);
 	}
 
