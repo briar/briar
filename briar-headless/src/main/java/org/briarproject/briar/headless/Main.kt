079c6e04751a653ac33933f8f84ed5805a885a3d
Sebastian Kürten <sebastian@mobanisto.de>
2023-06-20 12:21:11 +0200

    Add comment why we choose a differnt port for headless on macOS

--- briar-headless/src/main/java/org/briarproject/briar/headless/Main.kt
+++ briar-headless/src/main/java/org/briarproject/briar/headless/Main.kt
@@ -28,6 +28,7 @@ import java.util.logging.Level.INFO
 import java.util.logging.Level.WARNING
 import java.util.logging.LogManager
 
+// On macOS, port 7000 is used by ControlCenter (probably AirPlay), so use a different port
 private val DEFAULT_PORT = if (isMac()) 7001 else 7000
 private val DEFAULT_DATA_DIR = getProperty("user.home") + separator + ".briar"
 
